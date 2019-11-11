b531f42c8f71b5e3a796a15dcbff9bfa978c255c
serg-bloim <serg.bloim@gmail.com>
2019-11-11 16:53:08 -0500

    Replace 4 functions with a single function.

--- spell_duration_fix.cpp
+++ spell_duration_fix.cpp
@@ -1,12 +1,11 @@
-#define FTOL 0x005BF1AC
 void __declspec(naked) fix_spell_duration_overflow_0053973E()
 { // 0053973E
     __asm
     {
 		cmp		eax, 0xFFFF
-		jle		short ret_point1
+		jle		short ret_point
 		mov		eax, 0xFFFF
-ret_point1:
+ret_point:
 		// restore overridden code begin
 		mov     edx, [ebp-0xC]
 		mov     [edx+10h], ax
@@ -18,87 +17,18 @@ ret_point1:
 }
 // FUNCTIONS BELOW ARE DIFFERENT
 void __declspec(naked) fix_spell_duration_overflow()
-{ // 53BF9C
-    __asm
-    {
-		mov		eax, FTOL
-		call    eax
-		cmp		eax, 0xFFFF
-		jle		short ret_point
-		mov		eax, 0xFFFF
-ret_point:
-		ret
-    }
-}
-// FUNCTIONS BELOW ARE DIFFERENT
-void __declspec(naked) fix_spell_duration_overflow_0053BFA1()
 { // 0053BFA1
     __asm
     {
 		cmp		eax, 0xFFFF
-		jle		short ret_point2
+		jle		short ret_point
 		mov		eax, 0xFFFF
-ret_point2:
-		// restore overridden code begin
+ret_point:
 		mov     edx, [ebp-0x44]
 		mov     [edx+0x42], ax
-		// restore overridden code 
-		// jump back
-        mov     edx, 0x0053BFA8
-        jmp     edx
-    }
-}
-
-void __declspec(naked) fix_spell_duration_overflow_0053C0E1()
-{ // 0053C0E1
-    __asm
-    {
-		cmp		eax, 0xFFFF
-		jle		short ret_point3
-		mov		eax, 0xFFFF
-ret_point3:
-		// restore overridden code begin
-		mov     edx, [ebp-0x44]
-		mov     [edx+0x42], ax
-		// restore overridden code 
-		// jump back
-        mov     edx, 0x0053C0E8
-        jmp     edx
-    }
-}
-
-void __declspec(naked) fix_spell_duration_overflow_0053C21E()
-{ // 0053C21E
-    __asm
-    {
-		cmp		eax, 0xFFFF
-		jle		short ret_point4
-		mov		eax, 0xFFFF
-ret_point4:
-		// restore overridden code begin
-		mov     edx, [ebp-0x44]
-		mov     [edx+0x42], ax
-		// restore overridden code 
-		// jump back
-        mov     edx, 0x0053C225
-        jmp     edx
-    }
-}
-
-void __declspec(naked) fix_spell_duration_overflow_0053C52A()
-{ // 0053C52A
-    __asm
-    {
-		cmp		eax, 0xFFFF
-		jle		short ret_point5
-		mov		eax, 0xFFFF
-ret_point5:
-		// restore overridden code begin
-		mov     edx, [ebp-0x44]
-		mov     [edx+0x42], ax
-		// restore overridden code 
-		// jump back
-        mov     edx, 0x0053C531
-        jmp     edx
+		pop 	eax
+		inc 	eax
+		push 	eax
+		ret
     }
 }
