e7cc12ce765bbc0acf5b155ae68a0ca3252a113d
serg-bloim <serg.bloim@gmail.com>
2020-10-11 05:01:10 -0400

    Remove mail quests from inns

--- quests.cpp
+++ quests.cpp
@@ -1,13 +1,4 @@
-#include "syslib.h"
-#include "srvmgr_new.h"
 #include "config_new.h"
-#include "lib\utils.hpp"
-#include "srvmgrdef.h"
-#include "zxmgr.h"
-#include "cheat_codes_new.h"
-#include "player_info.h"
-#include "srvmgr.h"
-
 
 int __declspec(naked) imp_inn_quest_roll_interval()
 { // 0053693C
