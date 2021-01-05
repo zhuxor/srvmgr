f63ebabcb6e2ea2b2af8ad3eebbf813b00ca2dd6
igroglaz <39904162+igroglaz@users.noreply.github.com>
2021-01-05 22:06:42 +0300

    php scripts and exp reducing

---
+++ postbuild/_data/1.php
@@ -0,0 +1,72 @@
+<?php
+
+error_reporting(0);
+
+// EASY
+define(ORIGINAL_XML, "D:\GitHub\srvmgr\postbuild\_data\data_1easy.xml");
+define(OUTPUT_XML, "D:\GitHub\srvmgr\postbuild\_data\_data_1easy.xml");
+
+// EASY-2
+//define(ORIGINAL_XML, "D:\GitHub\srvmgr\postbuild\_data\data_2easy_medium.xml");
+//define(OUTPUT_XML, "D:\GitHub\srvmgr\postbuild\_data\_data_2easy_medium.xml");
+
+define(COEFFICIENT, 0.75);
+
+// functions
+
+function matchRULES($out) {
+	
+	if(preg_match("<experience>", $out)) {
+		$search = str_replace(array("<experience>", "</experience>"), "", $out);
+		$int = intval($search);
+		$newint = ($int != "-1") ? intval(COEFFICIENT * $int) : $int;
+		echo "Replacing: ".$int."->".$newint.PHP_EOL;
+		$out = str_replace($int, $newint, $out);
+	}
+		
+	return $out;
+	
+}
+
+function parseDATAxml($file, $msg = "Parsing data.bin xml...") {
+
+	$output = "";
+
+    echo $msg.PHP_EOL;
+
+    $read_thread = fopen($file, "r");
+
+    while ($buffer = fgets($read_thread, 128)) {
+      $output .= matchRULES($buffer);
+    }
+
+    if (!feof($read_thread)) {
+       echo "Error: unexpected fgets() fail" . PHP_EOL;
+    }
+
+    fclose($read_thread);
+
+	writeDATAxml(OUTPUT_XML, $output);
+
+    unset($buffer);
+	unset($output);
+	
+}
+
+function writeDATAxml($file, $buffer, $msg = "Writing new data.bin xml...") {
+    
+	echo $msg.PHP_EOL;
+	
+	$write_thread = fopen($file, "w");
+	fwrite($write_thread, $buffer, strlen($buffer));
+	fclose($write_thread);
+	
+}
+
+// run app
+
+parseDATAxml(ORIGINAL_XML);
+ 
+echo PHP_EOL.'Done.';
+
+?>
---
+++ postbuild/_data/php_a2.bat
@@ -0,0 +1,3 @@
+cd D:\GitHub\php
+php D:\GitHub\srvmgr\postbuild\_data\1.php
+pause
